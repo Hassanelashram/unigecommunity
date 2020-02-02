301da302fa51ba0d53fd19087754c0dbc2603c7b
Hassanelashram <elashramhassan@gmail.com>
2020-02-02 18:57:11 +0100

    devise working

--- db/migrate/20200202161937_create_add_name_to_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateAddNameToUsers < ActiveRecord::Migration[6.0]
-  def change
-    create_table :add_name_to_users do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_02_161937) do
-
-  create_table "add_name_to_users", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2020_02_02_163330) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -33,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_02_02_161937) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
