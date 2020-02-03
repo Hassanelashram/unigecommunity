d19709920df13c18928c7d024aa3201df58932d3
Hassanelashram <elashramhassan@gmail.com>
2020-02-03 20:24:44 +0100

    removed aws

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,10 @@ class Post < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
   validates :category, presence: true
+
+  has_attached_file :document
+  validates_attachment :document, content_type: { content_type: %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
+
+  has_attached_file :document_two
+  validates_attachment :document_two, content_type: { content_type: %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
 end
