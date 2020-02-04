f1a6647d2866c436d321a0c5d6571259e8755127
Hassanelashram <elashramhassan@gmail.com>
2020-02-04 20:49:35 +0100

    ohio

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,6 +5,7 @@ config.fog_credentials = {
 :provider => 'AWS',
 :aws_access_key_id => ENV['S3_ACCESS_KEY'],
 :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+:region => 'us-east-2'
 :path_style            => true
 }
     config.fog_directory     =  ENV['S3_BUCKET']
