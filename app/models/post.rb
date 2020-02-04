ebc0122cbd9e56ea3b2378fc2a117f7d65623de2
Hassanelashram <elashramhassan@gmail.com>
2020-02-04 17:06:16 +0100

    backto normal

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,9 @@ class Post < ApplicationRecord
   validates :user_id, presence: true
   validates :category, presence: true
 
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
   has_attached_file :document
   validates_attachment :document, content_type: { content_type: %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
 
