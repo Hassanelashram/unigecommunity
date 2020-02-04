e19c862597f4f3d99ea273d04a57d09a591bd38d
Hassanelashram <elashramhassan@gmail.com>
2020-02-04 19:26:41 +0100

    eu west 3

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,7 +5,7 @@ config.fog_credentials = {
 :provider => 'AWS',
 :aws_access_key_id => ENV['S3_ACCESS_KEY'],
 :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-:region                 => 'eu-west-1'
+:region                 => 'eu-west-3'
 }
     config.fog_directory     =  ENV['S3_BUCKET']
   end
