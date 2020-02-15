3d7de62284d4e7a02515ff4fd81c6f1996b05d04
Hassanelashram <elashramhassan@gmail.com>
2020-02-15 11:22:32 +0100

    validation removed for parent

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ApplicationRecord
   has_many :posts
   mount_uploader :catimage, CatimageUploader
-  belongs_to :parent, :class_name => 'Category'
+  validates :parent_id, presence: false
+  belongs_to :parent, :class_name => 'Category', optional: true
   has_many :children, :class_name => 'Category', :foreign_key => 'parent_id'
 end
