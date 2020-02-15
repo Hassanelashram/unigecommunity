a0b08bc5eef646fe99007e7e43290c0f10f12ceb
Hassanelashram <elashramhassan@gmail.com>
2020-02-15 10:50:10 +0100

    remove image validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,6 @@
 class Category < ApplicationRecord
   has_many :posts
   mount_uploader :catimage, CatimageUploader
-  validates :catimage, presence: true
   belongs_to :parent, :class_name => 'Category'
   has_many :children, :class_name => 'Category', :foreign_key => 'parent_id'
 end
