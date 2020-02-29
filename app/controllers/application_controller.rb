252dae71acf00ffd522010d0223508afce8cfdf6
Hassanelashram <elashramhassan@gmail.com>
2020-02-29 17:32:32 +0100

    before action posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-add_flash_types :success, :warning, :danger, :info
+  add_flash_types :success, :warning, :danger, :info
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:edit, :update, :destroy, :create, :new]
+  before_action :authenticate_user!, only: [:edit,:show, :update, :destroy, :create, :new]
 
 
   # GET /posts
